410adbed5660ba010dc3e1c06e133dc375707f0f
dhubmann <daniel.hubmann@gmx.at>
2023-07-21 12:30:19 +0200

    Fix collision bug
    Fixed misplacement of treasure object within walls.
    
    Also changed implementation of the edges and walls.

--- script.js
+++ script.js
@@ -17,29 +17,59 @@ const CELLWIDTH = canvas.width / COLS;
 const CELLHEIGHT = canvas.height / ROWS;
 
 let walls = [{ x: 0, y: 0, w: 1, h: 1 }];
+walls.push({ x: 0, y: 0, w: COLS, h: 1 }); // top
+walls.push({ x: 29, y: 0, w: 1, h: ROWS }); // right
+walls.push({ x: 0, y: 29, w: COLS, h: 1 }); // bottom
+walls.push({ x: 0, y: 0, w: 1, h: ROWS }); // right
+
+walls.push({ x: 2, y: 2, w: 26, h: 1 });
+walls.push({ x: 2, y: 3, w: 1, h: 25 });
+walls.push({ x: 4, y: 27, w: 24, h: 1 });
+walls.push({ x: 27, y: 4, w: 1, h: 22 });
+
+walls.push({ x: 4, y: 4, w: 22, h: 1 });
+walls.push({ x: 4, y: 6, w: 1, h: 21 });
+walls.push({ x: 6, y: 25, w: 20, h: 1 });
+walls.push({ x: 25, y: 6, w: 1, h: 20 });
+
+walls.push({ x: 6, y: 6, w: 18, h: 1 });
+walls.push({ x: 6, y: 8, w: 1, h: 16 });
+walls.push({ x: 7, y: 23, w: 15, h: 1 });
+walls.push({ x: 23, y: 8, w: 1, h: 16 });
+
+walls.push({ x: 8, y: 8, w: 14, h: 1 });
+walls.push({ x: 8, y: 9, w: 1, h: 13 });
+walls.push({ x: 10, y: 21, w: 13, h: 1 });
+walls.push({ x: 21, y: 8, w: 1, h: 12 });
+
 let player = { x: 10, y: 10 };
 let points = 0;
-let foundTreasure = false;
 let treasure;
 placeTreasure();
 
 // range of coordinates 1,1 - 28,28
 function placeTreasure() {
-  let randomX = Math.floor(Math.random() * (COLS - 2)) + 1;
-  let randomY = Math.floor(Math.random() * (ROWS - 2)) + 1;
+  while (true) {
+    let randomX = Math.floor(Math.random() * (COLS - 2)) + 1;
+    let randomY = Math.floor(Math.random() * (ROWS - 2)) + 1;
 
-  let treasureBounds = { x: randomX, y: randomY, w: 1, h: 1 };
+    let treasureBounds = { x: randomX, y: randomY, w: 1, h: 1 };
 
-  for (let wall of walls) {
-    if (collision(treasureBounds, wall)) return;
+    let colliding = false;
+    for (let wall of walls) {
+      if (collision(treasureBounds, wall)) {
+        colliding = true;
+        break;
+      }
+    }
+
+    if (!colliding) {
+      treasure = { x: randomX, y: randomY };
+      break;
+    }
   }
-
-  treasure = { x: randomX, y: randomY };
-  foundTreasure = false;
 }
 
-// treasure is newly placed every few seconds
-// TODO: treasure must not be placed "inside" labyrinth wall
 let timeInterval = 1;
 setInterval(gameLoop, timeInterval);
 
@@ -48,61 +78,34 @@ document.addEventListener("keydown", keyDown);
 draw();
 
 function draw() {
-  // canvas
   ctx.fillStyle = "turquoise";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // playing field
-  addPlayingField();
+  addWalls();
 
-  // walls
-  addWalls(2, 2, 26, 1);
-  addWalls(2, 3, 1, 25);
-  addWalls(4, 27, 24, 1);
-  addWalls(27, 4, 1, 22);
-
-  addWalls(4, 4, 22, 1);
-  addWalls(4, 6, 1, 21);
-  addWalls(6, 25, 20, 1);
-  addWalls(25, 6, 1, 20);
-
-  addWalls(6, 6, 18, 1);
-  addWalls(6, 8, 1, 16);
-  addWalls(7, 23, 15, 1);
-  addWalls(23, 8, 1, 16);
-
-  addWalls(8, 8, 14, 1);
-  addWalls(8, 9, 1, 13);
-  addWalls(10, 21, 13, 1);
-  addWalls(21, 8, 1, 12);
-
-  // player
   ctx.fillStyle = "white";
-  add(player.x, player.y);
+  add(player.x, player.y, 1, 1);
 
-  // treasure
   ctx.fillStyle = "yellow";
-  add(treasure.x, treasure.y);
+  add(treasure.x, treasure.y, 1, 1);
 
   requestAnimationFrame(draw);
 }
 
-function addWalls(x, y, w, h) {
-  walls.push({ x, y, w, h });
-  ctx.fillStyle = "black";
+function addWalls() {
+  for (let wall of walls) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+      wall.x * CELLWIDTH,
+      wall.y * CELLHEIGHT,
+      wall.w * CELLWIDTH,
+      wall.h * CELLHEIGHT
+    );
+  }
+}
+
+function add(x, y, w, h) {
   ctx.fillRect(x * CELLWIDTH, y * CELLHEIGHT, w * CELLWIDTH, h * CELLHEIGHT);
-}
-
-function addPlayingField() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, CELLHEIGHT); // top
-  ctx.fillRect(canvas.width - CELLWIDTH, 0, CELLWIDTH, canvas.height); // right
-  ctx.fillRect(0, canvas.height - CELLHEIGHT, canvas.width, CELLHEIGHT); // bottom
-  ctx.fillRect(0, 0, CELLWIDTH, canvas.height); // left
-}
-
-function add(x, y) {
-  ctx.fillRect(x * CELLWIDTH, y * CELLHEIGHT, CELLWIDTH, CELLHEIGHT);
 }
 
 // move player
@@ -149,7 +152,6 @@ function collision(rect1, rect2) {
 
 function gameLoop() {
   if (player.x == treasure.x && player.y == treasure.y) {
-    foundTreasure = true;
     points++;
     updateScore();
     placeTreasure();
