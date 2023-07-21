56790da7da06c8329516893ff6cd673166305ea5
dhubmann <daniel.hubmann@gmx.at>
2023-07-21 10:17:56 +0200

    Implement wall boundaries
    Added conditions so that the player object cannot move through and the treasure object cannot be placed inside labyrinth walls.
    
    Still buggy though as the calculation of the treasure object repeats
    until the treasure is placed on a free square while incrementing the score
    multiple times.

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ const COLS = 30;
 const CELLWIDTH = canvas.width / COLS;
 const CELLHEIGHT = canvas.height / ROWS;
 
-// player
-// treasure --> function placeTreasure
-// labyrinth
-
+let walls = [{ x: 0, y: 0, w: 1, h: 1 }];
 let player = { x: 10, y: 10 };
 let points = 0;
 let foundTreasure = false;
@@ -30,6 +27,12 @@ placeTreasure();
 function placeTreasure() {
   let randomX = Math.floor(Math.random() * (COLS - 2)) + 1;
   let randomY = Math.floor(Math.random() * (ROWS - 2)) + 1;
+
+  let treasureBounds = { x: randomX, y: randomY, w: 1, h: 1 };
+
+  for (let wall of walls) {
+    if (collision(treasureBounds, wall)) return;
+  }
 
   treasure = { x: randomX, y: randomY };
   foundTreasure = false;
@@ -49,101 +52,99 @@ function draw() {
   ctx.fillStyle = "turquoise";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // labyrinth
-  addLabyrinth();
+  // playing field
+  addPlayingField();
+
+  // walls
+  addWalls(2, 2, 26, 1);
+  addWalls(2, 3, 1, 25);
+  addWalls(4, 27, 24, 1);
+  addWalls(27, 4, 1, 22);
+
+  addWalls(4, 4, 22, 1);
+  addWalls(4, 6, 1, 21);
+  addWalls(6, 25, 20, 1);
+  addWalls(25, 6, 1, 20);
+
+  addWalls(6, 6, 18, 1);
+  addWalls(6, 8, 1, 16);
+  addWalls(7, 23, 15, 1);
+  addWalls(23, 8, 1, 16);
+
+  addWalls(8, 8, 14, 1);
+  addWalls(8, 9, 1, 13);
+  addWalls(10, 21, 13, 1);
+  addWalls(21, 8, 1, 12);
 
   // player
-  // TODO: change shape
   ctx.fillStyle = "white";
   add(player.x, player.y);
 
   // treasure
-  // TODO: change shape
   ctx.fillStyle = "yellow";
   add(treasure.x, treasure.y);
 
   requestAnimationFrame(draw);
 }
 
-function add(x, y) {
-  ctx.fillRect(x * CELLWIDTH, y * CELLHEIGHT, CELLWIDTH, CELLHEIGHT);
+function addWalls(x, y, w, h) {
+  walls.push({ x, y, w, h });
+  ctx.fillStyle = "black";
+  ctx.fillRect(x * CELLWIDTH, y * CELLHEIGHT, w * CELLWIDTH, h * CELLHEIGHT);
 }
 
-function addLabyrinth() {
+function addPlayingField() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, CELLHEIGHT); // top
   ctx.fillRect(canvas.width - CELLWIDTH, 0, CELLWIDTH, canvas.height); // right
   ctx.fillRect(0, canvas.height - CELLHEIGHT, canvas.width, CELLHEIGHT); // bottom
   ctx.fillRect(0, 0, CELLWIDTH, canvas.height); // left
-
-  // addLabyrinthWalls(2, 2, 26, 1);
-  // addLabyrinthWalls(2, 3, 1, 25);
-  // addLabyrinthWalls(4, 27, 24, 1);
-  // addLabyrinthWalls(27, 4, 1, 22);
-
-  // addLabyrinthWalls(4, 4, 22, 1);
-  // addLabyrinthWalls(4, 6, 1, 21);
-  // addLabyrinthWalls(6, 25, 20, 1);
-  // addLabyrinthWalls(25, 6, 1, 20);
-
-  // addLabyrinthWalls(6, 6, 18, 1);
-  // addLabyrinthWalls(6, 8, 1, 16);
-  // addLabyrinthWalls(7, 23, 15, 1);
-  // addLabyrinthWalls(23, 8, 1, 16);
-
-  // addLabyrinthWalls(8, 8, 14, 1);
-  // addLabyrinthWalls(8, 9, 1, 13);
-  // addLabyrinthWalls(10, 21, 13, 1);
-  // addLabyrinthWalls(21, 8, 1, 12);
-
-  // addLabyrinthWalls(10, 17, 2, 1);
-  // addLabyrinthWalls(10, 18, 1, 1);
-  // addLabyrinthWalls(12, 15, 3, 1);
-  // addLabyrinthWalls(12, 16, 1, 2);
-  // addLabyrinthWalls(14, 13, 3, 1);
-  // addLabyrinthWalls(14, 14, 1, 2);
-  // addLabyrinthWalls(16, 11, 3, 1);
-  // addLabyrinthWalls(16, 12, 1, 2);
-  // addLabyrinthWalls(18, 10, 1, 1);
 }
 
-function addLabyrinthWalls(x, y, w, h) {
-  ctx.fillRect(CELLWIDTH * x, CELLHEIGHT * y, CELLWIDTH * w, CELLHEIGHT * h); // top
-}
-
-function addLabyrinthWallsAtRandom() {
-  let randomX = Math.floor(Math.random() * (COLS - 2)) + 1;
-  let randomY = Math.floor(Math.random() * (ROWS - 2)) + 1;
-  let randomW = Math.floor(Math.random() * (12 - 2)) + 1;
-  let randomH = Math.floor(Math.random() * (12 - 2)) + 1;
-
-  ctx.fillRect(
-    CELLWIDTH * randomX,
-    CELLHEIGHT * randomY,
-    CELLWIDTH * randomW,
-    CELLHEIGHT * randomH
-  );
+function add(x, y) {
+  ctx.fillRect(x * CELLWIDTH, y * CELLHEIGHT, CELLWIDTH, CELLHEIGHT);
 }
 
 // move player
 function keyDown(e) {
+  let nextX = player.x;
+  let nextY = player.y;
+
   if (e.keyCode == 37) {
     if (player.x > 1) {
-      player.x--;
+      nextX--;
     }
   }
   if (e.keyCode == 38)
     if (player.y > 1) {
-      player.y--;
+      nextY--;
     }
   if (e.keyCode == 39)
     if (player.x < COLS - 2) {
-      player.x++;
+      nextX++;
     }
   if (e.keyCode == 40)
     if (player.y < ROWS - 2) {
-      player.y++;
+      nextY++;
     }
+
+  let playerBounds = { x: nextX, y: nextY, w: 1, h: 1 };
+
+  for (let wall of walls) {
+    if (collision(playerBounds, wall)) return;
+  }
+
+  player.x = nextX;
+  player.y = nextY;
+}
+
+function collision(rect1, rect2) {
+  return (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.y + rect1.h > rect2.y
+  );
 }
 
 function gameLoop() {
@@ -163,7 +164,7 @@ function testGameOver() {
   }
 }
 
-function updateScore(){
+function updateScore() {
   const scorePara = document.getElementById("score");
   scorePara.textContent = `Score: ${points}`;
 }
